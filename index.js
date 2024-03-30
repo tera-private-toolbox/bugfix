b1f23877eec2899942d35e5e98554d6f3794af8e
Dmitry S <dmitry.s@hsdn.org>
2024-03-30 07:07:06 +0400

    Auto remove old submodule files

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 class Bugfix {
     constructor(mod) {
-        const fs = require('fs'),
-              path = require('path'),
-              submoduleRoot = path.join(mod.info.path, 'lib');
+        const fs = require('fs');
+        const path = require('path');
+        const submoduleRoot = path.join(mod.info.path, 'lib');
 
+        // remove old submodule files
+        try {
+            const manifest = require('./manifest.json');
+            const manifestFiles = Object.keys(manifest.files)
+                .filter(submodule => submodule.includes('lib/'))
+                .map(submodule => submodule.replace('lib/', ''));
+
+            fs.readdirSync(submoduleRoot).forEach(submodule => {
+                if (!manifestFiles.includes(submodule)) {
+                    fs.unlinkSync(path.join(submoduleRoot, submodule));
+                }
+            });
+        } catch (_) {}
+
+        // load submodules
         this.submodules = fs.readdirSync(submoduleRoot).map(submodule => {
             const submoduleConstructor = require(path.join(submoduleRoot, submodule));
             return new submoduleConstructor(mod);
